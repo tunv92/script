db197508ab790fcb9435f2e00bbec3fbbfd172bd
tunv92 <59692722+tunv92@users.noreply.github.com>
2025-05-25 22:46:29 +0700

    Update go_1.24.sh

--- go_1.24.sh
+++ go_1.24.sh
@@ -4,5 +4,7 @@ curl -LO https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 tar -xzf go1.24.3.linux-amd64.tar.gz
 echo "export GO_HOME=$(pwd)/go/" >> ~/.bashrc
 echo 'export PATH=$GO_HOME/bin:$PATH' >> ~/.bashrc
+mkdir gopath
+echo 'export GOPATH=$(pwd)/gopath/' >> ~/.bashrc
 echo "Vui lòng chạy lệnh sau để cập nhật môi trường hiện tại:"
 echo "source ~/.bashrc"
