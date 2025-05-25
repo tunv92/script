f7c1d7edf6329233babea2351431010f7ece374c
tunv92 <59692722+tunv92@users.noreply.github.com>
2025-05-25 22:37:54 +0700

    Update go_1.24.sh

--- go_1.24.sh
+++ go_1.24.sh
@@ -2,7 +2,7 @@
 cd ~
 curl -LO https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 tar -xzf go1.24.3.linux-amd64.tar.gz
-echo "export JAVA_HOME=$(pwd)/amazon-corretto-21.0.7.6.1-linux-x64/" >> ~/.bashrc
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo "export GO_HOME=$(pwd)/go/" >> ~/.bashrc
+echo 'export PATH=$GO_HOME/bin:$PATH' >> ~/.bashrc
 echo "Vui lòng chạy lệnh sau để cập nhật môi trường hiện tại:"
 echo "source ~/.bashrc"
