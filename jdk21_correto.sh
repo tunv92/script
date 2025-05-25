5f6932795dd1751b7f2f8475b94e451f144878ba
tunv92 <59692722+tunv92@users.noreply.github.com>
2025-05-25 22:49:16 +0700

    Update jdk21_correto.sh

--- jdk21_correto.sh
+++ jdk21_correto.sh
@@ -4,5 +4,5 @@ curl -LO https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.
 tar -xzf amazon-corretto-21-x64-linux-jdk.tar.gz
 echo "export JAVA_HOME=$(pwd)/amazon-corretto-21.0.7.6.1-linux-x64/" >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
-echo "Vui lòng chạy lệnh sau để cập nhật môi trường hiện tại"
+echo "Please run this command to update environment vars:"
 echo "source ~/.bashrc"
